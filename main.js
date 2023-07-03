af6d764193f0eadd1d90239b46469b3c21160871
jeevashanmugam <jeevasj2000@gmail.com>
2023-07-03 21:59:32 +0530

    JS-student-mark-list

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let a = parseInt(prompt("Enter your Mark"));
+if (a < 35) {
+    document.write("You got fail mark")
+}
+else if (a < 70) {
+    document.write("You scored good mark")
+}
+else {
+    document.write("You got Outstanding mark")
+}
